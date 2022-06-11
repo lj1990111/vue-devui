26ba62dc1c4a9c04ccdcf06458057326c5b9582c
lj1990111 <luo15001635033@163.com>
2022-06-11 13:21:40 +0800

    feat(time-picker): add time picker size format

--- packages/devui-vue/devui/time-picker/src/time-picker.tsx
+++ packages/devui-vue/devui/time-picker/src/time-picker.tsx
@@ -1,121 +1,104 @@
-import { defineComponent, ref, onMounted, onUnmounted, watch } from 'vue';
+import { defineComponent, ref, onMounted, watch, SetupContext, Transition } from 'vue';
 import { TimePickerProps, timePickerProps } from './time-picker-types';
 import { Icon } from '../../icon';
 import useTimePicker from './composables/use-time-picker';
 import TimePopup from './components/time-popup/index';
+import DInput from '../../input/src/input';
+import { FlexibleOverlay } from '../../overlay';
+import { useNamespace } from '../../shared/hooks/use-namespace';
 
 import './time-picker.scss';
 
 export default defineComponent({
   name: 'DTimePicker',
-  components: { TimePopup },
+  components: { TimePopup, DInput },
   props: timePickerProps,
-  emits: ['selectedTimeChage','update:modelValue'],
-  setup(props: TimePickerProps, ctx) {
-
+  emits: ['change', 'update:modelValue'],
+  setup(props: TimePickerProps, ctx: SetupContext) {
+    const ns = useNamespace('time-picker');
     const activeHour = ref('00');
     const activeMinute = ref('00');
     const activeSecond = ref('00');
     const format = props.format.toLowerCase();
 
     const {
-      isActive,
       showPopup,
       devuiTimePicker,
       inputDom,
-      left,top,
+      overlayRef,
       showClearIcon,
       firsthandActiveTime,
       chooseTime,
-      getTimeValue,
       clickVerifyFun,
       isOutOpen,
-      vModelIsBeyond,
       clearAll,
       timePopupDom,
       vModeValue,
-      getPopupPosition
-    } = useTimePicker(activeHour,activeMinute,activeSecond,props.minTime,props.maxTime,format,props.autoOpen,props.disabled,props.modelValue);
+      changeTimeData,
+    } = useTimePicker(activeHour, activeMinute, activeSecond, format, props);
 
-
-    const selectedTimeChage = (e: MouseEvent) => {
-      isActive.value = false;
+    const selectedTimeChange = () => {
       showPopup.value = false;
-      ctx.emit('selectedTimeChage', vModeValue.value);
+      ctx.emit('change', vModeValue.value);
     };
-
     onMounted(() => {
-      getPopupPosition();
       isOutOpen();
-      vModelIsBeyond();
-      document.addEventListener('click', clickVerifyFun);
-      document.addEventListener('click',getTimeValue);
-      document.addEventListener('scroll',getPopupPosition);
-      window.addEventListener('resize',getPopupPosition);
-    });
-    onUnmounted(() => {
-      document.removeEventListener('click', clickVerifyFun);
-      document.removeEventListener('click',getTimeValue);
-      document.removeEventListener('scroll',getPopupPosition);
-      window.removeEventListener('resize',getPopupPosition);
     });
 
-    watch(vModeValue,(newValue: string)=>{
-      ctx.emit('update:modelValue',vModeValue.value);
-      if(newValue != props.minTime && newValue != '00:00'){
+    watch(vModeValue, (newValue: string) => {
+      ctx.emit('update:modelValue', vModeValue.value);
+      if (newValue !== props.minTime && newValue !== '00:00') {
         showClearIcon.value = true;
-      }else{
+      } else {
         showClearIcon.value = false;
       }
     });
 
     ctx.expose({
-      clearAll,chooseTime
+      clearAll,
+      chooseTime,
     });
 
     return () => {
       return (
-        <>
-          <div class={`devui-time-picker ${isActive.value ? 'time-picker-active' : ''} ${props.disabled ? 'picker-disabled' : ''}`}
-            ref={devuiTimePicker}
-          >
-            <TimePopup
-              ref={timePopupDom}
-              showPopup={showPopup.value}
-              popupTop={top.value}
-              popupLeft={left.value}
-              popupWidth={props.timePickerWidth}
-              popupFormat={ props.format.toLowerCase() }
-              minTime={props.minTime}
-              maxTime={props.maxTime}
-              bindData={firsthandActiveTime.value}
-              onSubData={selectedTimeChage}
-            >
-              {
-                ctx.slots.customViewTemplate?.()
-              }
-            </TimePopup>
-            <input ref={inputDom}
-              type="text"
-              value={vModeValue.value}
-              placeholder={`${props.placeholder}`}
+        <div class={`${ns.b()} ${props.disabled ? 'picker-disabled' : ''}`} ref={devuiTimePicker}>
+          <div ref={inputDom}>
+            <DInput
+              modelValue={vModeValue.value}
+              placeholder={props.placeholder}
               disabled={props.disabled}
-              class='time-input' />
-            <div class='time-input-icon'>
-              <div onClick={clearAll}>
-                {
-                  showClearIcon.value
-                    ? <Icon size="small" name="close" />
-                    :''
-                }
-              </div>
-              <div>
-                <Icon size="small" name="time" />
-              </div>
-            </div>
+              readonly={props.readonly}
+              size={props.size}
+              onFocus={clickVerifyFun}
+              v-slots={{
+                suffix: () => (
+                  <span class="time-input-icon">
+                    <span onClick={clearAll} class="clear-button">
+                      {showClearIcon.value ? <Icon size="small" name="close" /> : ''}
+                    </span>
+                    <Icon size="small" name="time" />
+                  </span>
+                ),
+              }}></DInput>
           </div>
-        </>
+          <Transition name="fade">
+            <FlexibleOverlay v-model={showPopup.value} ref={overlayRef} origin={inputDom.value}>
+              <TimePopup
+                ref={timePopupDom}
+                showPopup={showPopup.value}
+                popupWidth={props.timePickerWidth}
+                popupFormat={props.format.toLowerCase()}
+                minTime={props.minTime}
+                maxTime={props.maxTime}
+                bindData={firsthandActiveTime.value}
+                onSubmitData={selectedTimeChange}
+                onChange={changeTimeData}>
+                {ctx.slots.customViewTemplate?.()}
+              </TimePopup>
+            </FlexibleOverlay>
+          </Transition>
+        </div>
       );
     };
-  }
+  },
 });
--- packages/devui-vue/devui/time-picker/src/types.ts
+++ packages/devui-vue/devui/time-picker/src/types.ts
@@ -1,3 +1,4 @@
+import { Ref } from 'vue';
 
 export type timeType = 'hh' | 'HH' | 'mm' | 'MM' | 'ss' | 'SS';
 export type TimeObj = {
@@ -5,10 +6,54 @@ export type TimeObj = {
   type?: timeType;
 };
 
+export type timeDataType = 'hour' | 'minute' | 'second';
+
 export type ArrType = {
-  type: 'hour' | 'minute' | 'seconde';
+  type: timeDataType;
   isActive: boolean;
   isDisabled: boolean;
   time: string;
   flag: string;
+};
+
+export interface TimeListItem {
+  hh: ArrType[];
+  mm: ArrType[];
+  ss: ArrType[];
+}
+export interface popupTimeObj {
+  activeHour: Ref<string>;
+  activeMinute: Ref<string>;
+  activeSecond: Ref<string>;
+}
+
+export type sizeType = 'lg' | 'md' | 'sm';
+
+export type UseTimerPickerFn = {
+  showPopup: Ref<boolean>;
+  devuiTimePicker: Ref<HTMLElement | undefined>;
+  timePickerValue: Ref<string>;
+  inputDom: Ref<HTMLElement | undefined>;
+  overlayRef: Ref<HTMLElement | undefined>;
+  timePopupDom: Ref<HTMLElement | undefined>;
+  showClearIcon: Ref<boolean>;
+  firsthandActiveTime: Ref<string>;
+  vModeValue: Ref<string>;
+  getTimeValue: (e: MouseEvent) => void;
+  clickVerifyFun: (e: MouseEvent) => void;
+  isOutOpen: () => void;
+  clearAll: (e: MouseEvent) => void;
+  chooseTime: (slotTime: TimeObj) => void;
+  changeTimeData: ({ activeHour, activeMinute, activeSecond }: popupTimeObj) => void;
+};
+
+export type UsePopupLineFn = {
+  activeTime: Ref<string>;
+  activeHour: Ref<string>;
+  activeMinute: Ref<string>;
+  activeSecond: Ref<string>;
+  activeTimeFun: (e: any, item: ArrType, index: number) => void;
+  resetTimeValue: (time: string) => void;
+  getNewTime: () => void;
+  resetScrollTop: () => void;
 };
